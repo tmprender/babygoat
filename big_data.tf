002bb4fe55afbedf961a8acf973abf66cb6e6243
Tom Prenderville <tprendervill@paloaltonetworks.com>
2022-05-12 19:56:16 -0400

    removed labels from big_data.tf

--- big_data.tf
+++ big_data.tf
@@ -25,11 +25,7 @@ resource "google_bigquery_dataset" "dataset" {
     role          = "READER"
   }
   labels = {
-    git_commit           = "2bdc0871a5f4505be58244029cc6485d45d7bb8e"
     git_file             = "terraform__gcp__big_data_tf"
-    git_last_modified_at = "2022-01-19-17-02-27"
-    git_last_modified_by = "jameswoolfenden"
-    git_modifiers        = "jameswoolfenden__nimrodkor"
     git_org              = "tmprender"
     git_repo             = "terragoat"
     yor_trace            = "2560d883-bc3a-4cb6-b9fc-fb666edf626e"
