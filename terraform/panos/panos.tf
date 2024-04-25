e21ed17837b9f31a531882fa37571dba7ddcf8bd
tmprender <tmprender@gmail.com>
2024-04-25 16:55:08 -0400

    test panos.tf

--- terraform/panos/panos.tf
+++ terraform/panos/panos.tf
@@ -12,8 +12,23 @@ provider "panos" {
     api_key = "secret"
 }
 
-data "panos_security_rule" "example" {
+data "panos_security_rule" "rule1" {
     name = "my rule name"
 
-    tags = [{"k":"v"}]
+    tags = ["test","dev"]
+
+}
+
+resource "panos_address_object" "example" {
+    name = "localnet"
+    value = "192.168.80.0/24"
+    description = "The 192.168.80 network"
+    tags = [
+        "internal",
+        "dmz",
+    ]
+
+    lifecycle {
+        create_before_destroy = true
+    }
 }
